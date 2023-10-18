5a997adac091ae0a46a77f3538284122b9c182a8
[southixa phlavong] <[southixa.pele10@gmail.com]>
2023-10-18 19:41:54 +0700

    first commit

---
+++ resources/js/router/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vue app</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos13App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
